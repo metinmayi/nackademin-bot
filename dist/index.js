9fa63947c971704d5b0469585c8dd83890411187
Metin Ahmad <metin.ahmad@paligo.net>
2023-01-25 22:04:51 +0100

    Added compiled files

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+import { Client, Events, GatewayIntentBits, Partials } from "discord.js";
+import dotenv from "dotenv";
+import { HelpQueue } from "./HelpQueue.js";
+dotenv.config();
+const token = process.env.BOT_TOKEN || "";
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessageReactions],
+    partials: [Partials.User, Partials.GuildMember, Partials.Reaction],
+});
+client.login(token);
+client.once(Events.ClientReady, (c) => {
+    new HelpQueue(client).initiate();
+});
---
+++ dist/mainEmbed.js
@@ -0,0 +1,22 @@
+import { EmbedBuilder } from "@discordjs/builders";
+export function getMainEmbed(helpQueue) {
+    const students = helpQueue.queue;
+    const studentFields = students.map((student) => {
+        return {
+            name: " ",
+            value: student.username,
+        };
+    });
+    return new EmbedBuilder()
+        .setColor(0x0099ff)
+        .setAuthor({ name: "Kö för handledning" })
+        .setThumbnail("https://api.yhguiden.se/files/school/logo/219/nackademin_2022.jpg")
+        .setDescription(`Reagera med :tickets: för att ta en kölapp.\nReagera en gång till för att gå ur kön.`)
+        .addFields([
+        {
+            name: "Kö:",
+            value: " ",
+        },
+        ...studentFields,
+    ]);
+}
